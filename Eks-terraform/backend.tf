c7944ad84d32a8538f632937450ecf71934c059b
savithaprakash289 <136881941+savithaprakash289@users.noreply.github.com>
2024-03-08 17:07:56 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "savithareddit" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "mumbai23112023/terraform.tfstate"
     region = "ap-south-1"
   }
 }
